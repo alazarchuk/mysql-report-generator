dafc6aaa07c2737d7f676c7fb5d97c11573bfba2
Andrii Lazarchuk <al@ext.chime.com>
2020-04-15 19:05:30 +0300

    Add mysqlmonlite

--- notes.sh
+++ notes.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt-get install libdbd-mysql-perl
+sudo apt install mysql-client
 
 ./mysqlreport --no-mycnf --user admin --pasword '' --host database-1.cer8saahyxyj.us-east-2.rds.amazonaws.com
