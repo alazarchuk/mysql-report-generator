3f02ddb6f5202b55b9ed6fd181cd7faf6a9f1e28
Andrii Lazarchuk <al@ext.chime.com>
2020-04-16 20:17:37 +0300

    Remove unused file

--- notes.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-sudo apt-get install libdbd-mysql-perl
-sudo apt install mysql-client
-
-./mysqlreport --no-mycnf --user admin --pasword '' --host database-1.cer8saahyxyj.us-east-2.rds.amazonaws.com
