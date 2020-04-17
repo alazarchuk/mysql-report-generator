de098ad1b19fbb8c8f8333fd874e2b04767b3447
Andrii Lazarchuk <al@ext.chime.com>
2020-04-17 17:43:17 +0300

    Add mysql-report-replay-repeat

---
+++ mysql-report-replay-repeat.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#######################################################
+# Copyright (C) 2020
+# Program: mysqlmymonlite.sh
+# MySQL benchmark and monitoring script 
+# by Andrii Lazarchuk
+
+TRIES="1"
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -t|--tries)
+    TRIES="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -f|--slow-query-log-file)
+    LOGFILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+# TBD: Verify log file exist
+# Output DBs list and expose environment variables to code below
+echo '======================'
+echo 'DB List'
+echo '======================'
+. ./mysqlmymonlite.sh dblist
+echo ''
+
+while [ $TRIES -gt 0 ]
+do
+echo '======================'
+echo "Try #$TRIES"
+echo '======================'
+./mysqlmymonlite.sh mysql > mysql-monitor-report-$TRIES.log 2>&1
+percona-playback --mysql-max-retries 1 --mysql-host $MYSQLHOST --query-log-file $LOGFILE > mysql-playback-$TRIES.log 2>&1
+i=$[$i-1]
+done
