bccd028fd174ec75f84cbd3a300ad0dcaa84b4d7
Andrii Lazarchuk <al@ext.chime.com>
2020-04-21 12:13:29 +0300

    Add threads option

--- mysql-report-replay-repeat.sh
+++ mysql-report-replay-repeat.sh
@@ -5,7 +5,8 @@
 # MySQL benchmark and monitoring script 
 # by Andrii Lazarchuk
 
-TRIES="1"
+REPEATS="1"
+THREADS="1"
 REPORT_PATH="reports/$(date "+%m%d%H%M%Y.%S")"
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -13,8 +14,13 @@ do
 key="$1"
 
 case $key in
-    -t|--tries)
-    TRIES="$2"
+    -r|--repeat)
+    REPEATS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--threads)
+    THREADS="$2"
     shift # past argument
     shift # past value
     ;;
@@ -54,7 +60,7 @@ mkdir $REPORT_PATH
 # TBD: Verify if ~/.my.cnf exists
 
 CURRENT_TRY='1'
-while [ $TRIES -ge $CURRENT_TRY ]
+while [ $REPEATS -ge $CURRENT_TRY ]
 do
 echo '======='
 echo "Try #$CURRENT_TRY"
@@ -66,6 +72,7 @@ export DATABASE
 ./mysqlmymonlite.sh mysql > $REPORT_DEST 2>&1
 echo "Report saved to $REPORT_DEST"
 echo 'Start queries playback'
+echo "Run $THREADS threads in parallel"
 REPORT_DEST="$REPORT_PATH/mysql-playback-$CURRENT_TRY.log"
 percona-playback --mysql-max-retries 1 \
                  --mysql-host $MYSQLHOST \
@@ -73,7 +80,7 @@ percona-playback --mysql-max-retries 1 \
                  --mysql-username $MYSQLUSER \
                  --mysql-password $MYSQLPASSWORD \
                  --query-log-file $LOGFILE > $REPORT_DEST 2>&1
-echo "Report saved to $REPORT_DEST"
+echo "Writing report to $REPORT_DEST"
 echo ''
 CURRENT_TRY=$[$CURRENT_TRY+1]
 done
